c97c122db8e660a3c8a9a7d6ee36031340071bac
Rob Golding <rob@robgolding.com>
2013-02-20 10:02:08 +0000

    Add setup.py to repo

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='celery-s3',
+    version='0.1',
+    description='An S3 result store backend for Celery',
+    long_description=open('README.md').read(),
+    author='Rob Golding',
+    author_email='rob@robgolding.com',
+    license='BSD',
+    url='https://github.com/robgolding63/celery-s3',
+    download_url='https://github.com/robgolding63/celery-s3/downloads',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: System :: Distributed Computing',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+    ],
+)
