081478368d3cde75839b70df46e460ac3232afae
AUCHLY1 <lydia.auch@novartis.com>
2017-11-21 10:39:50 -0500

    Made code formatting consistent with repo

--- celery_s3/backends/s3.py
+++ celery_s3/backends/s3.py
@@ -82,7 +82,7 @@ class S3Backend(KeyValueStoreBackend):
         return self._get_key(key).set_contents_from_string(
             value,
             reduced_redundancy=self.reduced_redundancy,
-	    encrypt_key=self.encrypt_key
+	    encrypt_key=self.encrypt_key,
         )
 
     def delete(self, key):
